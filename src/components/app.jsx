a3cde011d1acc1e212d5b12273cb4aff47b22165
Dan Menza <dan.menza@aeropay.com>
2023-08-23 14:24:17 -0500

    Built flat, flatList, and marker components. Built selecting flat functionality and added default list of flats under data folder.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-const App = () => {
-  return (
-    <div className="app">
-      <p>React + Redux starter</p>
-    </div>
-  );
-};
+import flats from './../data/flats';
+import FlatList from './flatList';
+import Marker from './marker';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.selectFlat = this.selectFlat.bind(this);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
